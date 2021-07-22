50c06e3460b2458a684c8c7a0faf337fa03e2719
Matanya Loewenthal <matanya.loewenthal@gmail.com>
2021-07-22 11:04:29 -0600

    set TODO and made basic layout of methods

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+
+# TODO 
+# method that takes student ID, Event ID, and Registration ID and returns unique QR code
+
+def QR_gen(Student_ID, Event_ID, Registration_ID):
+    QR = ""
+    return QR
+
+# TODO
+# method that takes QR code and email address and sends ticket email. 
+# also return registration object with info
+
+def send_ticket(QR_image, email):
+    # use aAPI to send ticket to registrant
+    registration_obj = "" # object containing name and ID (for now)
+    return registration_obj
+
+# TODO
+# method that makes string html map from gmplot with heatmap of events
+
+def heatmap_gen():
+    heatmap_html_string = ""
+    return heatmap_html_string
+
+# TODO
+# method that takes registration object and google sheet and adds to google sheet
+
+def register(registration_obj, google_sheet_name):
+    # add registrant to google sheet
+    return None
+
