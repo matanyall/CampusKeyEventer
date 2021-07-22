ae02c2fcda0b00206db0012d01281ce428c17499
Eric Zhou <ezhou@Erics-MacBook-Air.local>
2021-07-22 15:45:53 -0500

    fixed errors with styles

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, session
 from pathlib import Path
-
+debug=True # set this to false later;
 app = Flask(__name__)
 
 # Most safe practice amirite...
