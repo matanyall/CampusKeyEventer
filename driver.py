6d50c70fa107c34971a1113d752c1210bc0ef176
Matanya Loewenthal <matanya.loewenthal@gmail.com>
2021-07-23 12:44:00 -0600

    set driver for presentation

--- driver.py
+++ driver.py
@@ -4,9 +4,9 @@ main.make_student("firstname lastname", "12345", "firstname@corn.com")
 
 # print(main.student_list)
 
-# main.register_for_event("Matanya Loewenthal", "115353015", "matanya@loewenthal.net", "CampusKeyHackathon")
+main.register_for_event("Matanya Loewenthal", "115353015", "matanya@loewenthal.net", "CampusKeyHackathon")
 
 # main.register_for_event_qr_only("Matanya Loewenthal", "125353015", "matanya@loewenthal.net", "CampusKeyHackathon")
 
-main.heatmap_gen()
+# main.heatmap_gen()
 
