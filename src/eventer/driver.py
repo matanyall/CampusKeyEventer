f7a7247464348e8bdbdf59d7fbc0bfd0033f829f
Matanya Loewenthal <matanya.loewenthal@gmail.com>
2021-07-26 12:02:08 -0600

    made all the code live in src/

---
+++ src/eventer/driver.py
@@ -0,0 +1,12 @@
+import main
+
+main.make_student("firstname lastname", "12345", "firstname@corn.com")
+
+# print(main.student_list)
+
+main.register_for_event("Matanya Loewenthal", "115353015", "matanya@loewenthal.net", "CampusKeyHackathon")
+
+# main.register_for_event_qr_only("Matanya Loewenthal", "125353015", "matanya@loewenthal.net", "CampusKeyHackathon")
+
+# main.heatmap_gen()
+
---
+++ src/flask_files/static/js/home.js
@@ -0,0 +1,11 @@
+$(document).ready( () => {
+    
+    $('#event-big-span').on('click', event => {
+        window.location.replace('/events');
+    });
+
+    $('#map-big-span').on('click', event => {
+        window.location.replace('/map');
+    });
+
+});
